a4da52ee50ccba6f159c40e9b307adec05e86932
Mohammed Moajez <lmoajez@gmail.com>
2025-04-17 22:24:53 +0530

    newtwo

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from './config/db';
-import userRoutes from './routes/userRoutes';
-import syllabusRoutes from './routes/syllabusRoutes';
+import connectDB from './src/config/db';
+import userRoutes from './src/routes/userRoutes';
+import syllabusRoutes from './src/routes/syllabusRoutes';
 import { error } from 'console';
 
 dotenv.config();
@@ -15,7 +15,6 @@ connectDB();
 
 app.use(express.json());
 app.use(cors());
-
 // Configure CORS
 app.use(cors({
   origin: 'http://localhost:5173', // Allow requests from this origin
